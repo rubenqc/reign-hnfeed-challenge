31eaaa7185cf5b07bcba586c0db2cc22945f64bc
Ruben Quispe <rwquispec@uni.pe>
2021-06-16 23:12:14 -0500

    setup backend docker-compose

--- server/src/database/database.module.ts
+++ server/src/database/database.module.ts
@@ -1,5 +1,4 @@
 import { Module, Global } from '@nestjs/common';
-import { MongoClient } from 'mongodb';
 import { ConfigType } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
@@ -21,21 +20,6 @@ import config from '../config';
       },
       inject: [config.KEY],
     }),
-  ],
-  providers: [
-    {
-      provide: 'MONGO',
-      useFactory: async (configService: ConfigType<typeof config>) => {
-        const { connection, username, password, host, port, dbName } =
-          configService.database;
-        const uri = `${connection}://${username}:${password}@${host}:${port}/?authSource=admin&readPreference=primary`;
-        const client = new MongoClient(uri);
-        await client.connect();
-        return client.db(dbName);
-      },
-      inject: [config.KEY],
-    },
-  ],
-  exports: ['MONGO', MongooseModule],
+  ]
 })
 export class DatabaseModule {}
